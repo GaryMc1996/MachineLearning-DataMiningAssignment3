7bd463cbe240014a53fb01fa25fa83773ba5a59e
Gary McMahon <g.mcmahon5@nuigalway.ie>
2017-11-23 19:59:05 +0000

    Update file, refractored code and removed redundant logistic function. Every works with 100% accuracy

--- MultinomialLogisticRegression.py
+++ MultinomialLogisticRegression.py
@@ -1,14 +1,14 @@
 import sys
-import numpy
+import numpy as np
 import pandas as panda
 import pandas as pd 
 from tkinter.filedialog import askopenfilename
 import math as Math
 
-numpy.seterr(all='ignore')
+np.seterr(all='ignore')
  
 def sigmoid(x):
-    return 1. / (1 + numpy.exp(-x))
+    return 1. / (1 + np.exp(-x))
 def getAccuracy(predicted, actual):
     accuracy = 0
     maxAccuracyScore = len(predicted)
@@ -24,7 +24,7 @@ def getAccuracy(predicted, actual):
     return accuracy/maxAccuracyScore
 def oneHotEncoding(owlData,):
     #oneHotData = owlData[:,4]
-    types = numpy.unique(owlData)
+    types = np.unique(owlData)
     alphabet = "LongEaredOwl","SnowyOwl","BarnOwl"
 
     char_to_int = dict((c, i) for i, c in enumerate(alphabet))
@@ -47,16 +47,16 @@ def oneHotEncoding(owlData,):
 
 def maxValToOne(data):
     #create array of 0's the same size as the current array
-    correctData= numpy.zeros_like(data)
-    correctData[numpy.arange(len(data)), data.argmax(1)] = 1
+    correctData= np.zeros_like(data)
+    correctData[np.arange(len(data)), data.argmax(1)] = 1
     return correctData
 
 def scaleData(data):
     i=0
     while(i<3):
         #find max and min value of array
-        maxVal = numpy.amax(data[:,i])
-        minVal = numpy.amin(data[:,i])
+        maxVal = np.amax(data[:,i])
+        minVal = np.amin(data[:,i])
         j=0
         while(j<len(data)):
             #compute new normailsed value and round to 4 decimals places. still ensures accuracy 
@@ -70,8 +70,8 @@ def normaliseData(data):
     i=0
     while(i<4):
         #find max and min value of array
-        maxVal = numpy.amax(data[:,i])
-        minVal = numpy.amin(data[:,i])
+        maxVal = np.amax(data[:,i])
+        minVal = np.amin(data[:,i])
         j=0
         while(j<len(data)):
             #compute new normailsed value and round to 4 decimals places. still ensures accuracy 
@@ -80,43 +80,28 @@ def normaliseData(data):
             j = j+1
         i=i+1
     return data
-def softmax(x):
-    e = numpy.exp(x - numpy.max(x))  # prevent overflow
+def softmax(z):
+    e = np.exp(z - np.max(z))  # prevent overflow
     if e.ndim == 1:
-        return e / numpy.sum(e, axis=0)
+        return e / np.sum(e, axis=0)
     else:  
-        return e / numpy.array([numpy.sum(e, axis=1)]).T  # ndim = 2
+        return e / np.array([np.sum(e, axis=1)]).T  # ndim = 2
 
 
-class LogisticRegression(object):
-    def __init__(self, input, label, n_in, n_out):
-        self.x = input
-        self.y = label
-        self.W = numpy.zeros((n_in, n_out))  # initialize W 0
-        self.b = numpy.zeros(n_out)          # initialize bias 0
 
-        # self.params = [self.W, self.b]
-
-    def train(self, lr=0.1, input=None, L2_reg=0.00):
-        if input is not None:
-            self.x = input
-
-        # p_y_given_x = sigmoid(numpy.dot(self.x, self.W) + self.b)
-        p_y_given_x = softmax(numpy.dot(self.x, self.W) + self.b)
-        d_y = self.y - p_y_given_x
+def train(features, target,weights, bias, alpha,L2_reg=0.00):
+    # p_y_given_x = sigmoid(np.dot(self.x, self.W) + self.b)
+    p_y_given_x = softmax(np.dot(features, weights) + bias)
+    d_y = target - p_y_given_x
         
-        self.W += lr * numpy.dot(self.x.T, d_y) - lr * L2_reg * self.W
-        self.b += lr * numpy.mean(d_y, axis=0)
-
+    weights += alpha * np.dot(features.T, d_y) - alpha * L2_reg * weights
+    bias += alpha * np.mean(d_y, axis=0)
+    return weights,bias
         # cost = self.negative_log_likelihood()
         # return cost
-
-
-
-
-    def predict(self, x):
-        # return sigmoid(numpy.dot(x, self.W) + self.b)
-        return softmax(numpy.dot(x, self.W) + self.b)
+def predict(features, weights, bias):
+ # return sigmoid(np.dot(x, self.W) + self.b)
+    return softmax(np.dot(features, weights) + bias)
 
 
 def test_lr(learning_rate=0.01, n_epochs=200):
@@ -124,11 +109,11 @@ def test_lr(learning_rate=0.01, n_epochs=200):
     #open window that allows user to select file to test. 
     #fileName = askopenfilename()
     #open the csv file. We are assuming the user defined the headers for the data in the dataset
-    owlData=numpy.array(panda.read_csv("owls15.csv",header = None))
+    owlData=np.array(panda.read_csv("owls15.csv",header = None))
     #remove headers 
     headerData = owlData[0]
     owlData = owlData[1:]
-    numpy.random.shuffle(owlData)
+    np.random.shuffle(owlData)
     #find the number of columns in the array, We are always going to assume that the last column holds the type 
     #i.e Barn owl, Snowy owl etc. Using this we will assume the rest of the columns to be feature data 
     typeColumn = (len(owlData[0])) - 1
@@ -139,7 +124,7 @@ def test_lr(learning_rate=0.01, n_epochs=200):
     trainLength = Math.floor(len(owlData)/3)
     targetData = owlData[:,typeColumn]
 
-    targetData = numpy.array(oneHotEncoding(targetData))
+    targetData = np.array(oneHotEncoding(targetData))
     #print(targetData)
     #print(targetData) 
     #targetData  = oneHotEncoding(targetData)
@@ -152,29 +137,34 @@ def test_lr(learning_rate=0.01, n_epochs=200):
     #print(featureData)
     
 
-    x = numpy.array(trainFeatureData,dtype=numpy.float32)
+    x = np.array(trainFeatureData,dtype=np.float32)
    
-    y = numpy.array(trainTargetData,dtype=numpy.float32) 
-
-    # construct LogisticRegression
-    classifier = LogisticRegression(input=x, label=y, n_in=4, n_out=3)
+    y = np.array(trainTargetData,dtype=np.float32) 
+    #find number of features in the dataset
+    numFeatures = len(x.T)
+   #find the number of ccategories in the dataset
+    numCategories = len(y.T)
+   
+    weightMatrix = np.zeros((numFeatures, numCategories))  
+    biasMatrix = np.zeros(numCategories) 
 
     # train
     for epoch in range(n_epochs):
-        classifier.train(lr=learning_rate)
-        
+        weights,bias = train(x,y,weightMatrix,biasMatrix,alpha=learning_rate)
         if (epoch % 100 ==0):
             print('Training epoch %d, cost is ' % epoch)
         learning_rate *= 0.95
+        weightMatrix= weights
+        biasMatrix =bias
 
 
     # test
-    x1 = numpy.array(testFeatureData,dtype=numpy.float32)
-    y1 = numpy.array(testTargetData,dtype=numpy.float32)
+    x1 = np.array(testFeatureData,dtype=np.float32)
+    y1 = np.array(testTargetData,dtype=np.float32)
    
-    data = (classifier.predict(x1))
+    data = predict(x1,weightMatrix,biasMatrix)
     data = maxValToOne(data)
-    #print(numpy.c_[data, y])
+    print(np.c_[data, y1])
     accuracy  = getAccuracy(data,y1)
     print(accuracy)
     #print(len(data))
